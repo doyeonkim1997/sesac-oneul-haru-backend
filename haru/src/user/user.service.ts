23b92e454f0f13d2bfa0ace4fdf15d4288de038b
WARDKEY <tjrmsduq@naver.com>
2025-07-23 18:48:25 +0900

    feat(user): 사용자id로 친구 목록 조회 기능 추가

--- haru/src/user/user.service.ts
+++ haru/src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { FindUserDto } from './dto/find-user-dto';
 import { UserRepository } from './user.repository';
+import { FindFriendDto } from './dto/find-friend-dto';
 
 @Injectable()
 export class UserService {
@@ -11,5 +12,10 @@ export class UserService {
     const users = await this.userRepository.findUserByNickName(search);
 
     return users;
+  }
+
+  // 사용자 id로 친구 목록 조회
+  async getFriendsByUserId(userId: number): Promise<FindFriendDto[]> {
+    return this.userRepository.findFriendsByUserId(userId);
   }
 }
