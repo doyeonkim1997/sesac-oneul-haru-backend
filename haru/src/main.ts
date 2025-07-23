10ad1617bfb92cf1264f9d47a64a4c7c0093c7fc
WARDKEY <tjrmsduq@naver.com>
2025-07-23 15:09:20 +0900

    feat(swagger, validator): swagger와 validator 설정 추가

--- haru/src/main.ts
+++ haru/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 유효성 검사 전역으로 사용
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger 관련 세팅
+  const options = new DocumentBuilder()
+    .setTitle('Haru Project')
+    .setDescription('Haru 프로젝트 API 문서')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api-docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
