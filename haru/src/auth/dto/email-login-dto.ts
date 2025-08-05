19db79d3dc9325a8518c5c9f3aafa16e94139a3a
WARDKEY <tjrmsduq@naver.com>
2025-08-05 13:09:58 +0900

    fix(password): 비밀번호 유효성 검사 수정

--- haru/src/auth/dto/email-login-dto.ts
+++ haru/src/auth/dto/email-login-dto.ts
@@ -22,7 +22,7 @@ export class EmailLoginDto {
   @IsNotEmpty()
   @MinLength(8) // 최소 8자
   @MaxLength(20) // 최대 20자
-  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/, {
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\W_]+$/, {
     message: '비밀번호는 문자와 숫자를 각각 1개 이상 포함해야 합니다.',
   })
   password: string;
--- haru/src/user/dto/update-password-dto.ts
+++ haru/src/user/dto/update-password-dto.ts
@@ -12,7 +12,7 @@ export class UpdatePasswordDto {
   @IsNotEmpty()
   @MinLength(8)
   @MaxLength(20)
-  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/, {
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\W_]+$/, {
     message: '비밀번호는 문자와 숫자를 각각 1개 이상 포함해야 합니다.',
   })
   currentPassword: string;
@@ -27,7 +27,7 @@ export class UpdatePasswordDto {
   @IsNotEmpty()
   @MinLength(8) // 최소 8자
   @MaxLength(20) // 최대 20자
-  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/, {
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\W_]+$/, {
     message: '비밀번호는 문자와 숫자를 각각 1개 이상 포함해야 합니다.',
   })
   newPassword: string;
@@ -42,7 +42,7 @@ export class UpdatePasswordDto {
   @IsNotEmpty()
   @MinLength(8)
   @MaxLength(20)
-  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+$/, {
+  @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\W_]+$/, {
     message: '비밀번호는 문자와 숫자를 각각 1개 이상 포함해야 합니다.',
   })
   confirmPassword: string;
